3316830c51732d3b7ad769b03405762345611102
Juan Pablo Orizabal <juanpablo-orizabalgil@cunoc.edu.gt>
2020-08-07 22:53:46 -0600

    PruebaInicial

--- SisAdminBancaJ3B/src/sisadminbancaj3b/SisAdminBancaJ3B.java
+++ SisAdminBancaJ3B/src/sisadminbancaj3b/SisAdminBancaJ3B.java
@@ -13,7 +13,7 @@ public class SisAdminBancaJ3B {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
