69bec06f1ec0d47ee10bb5b3950fa051c6799723
BryanBmmF <bryan.bmmf@gmail.com>
2020-08-07 22:24:59 -0600

    Creando Estructura Base para la App y el SisAdmin

---
+++ SisAdminBancaJ3B/src/sisadminbancaj3b/SisAdminBancaJ3B.java
@@ -0,0 +1,19 @@
+/*
+ * Clase Principal
+ */
+package sisadminbancaj3b;
+
+/**
+ *
+ * @author bryan
+ */
+public class SisAdminBancaJ3B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
