e8fb8a719426b0abed03db97b3360f76dc116157
Jesfrin Gonzalez <JesfrinGnZ>
2020-08-13 09:03:10 -0600

    logica para CRUD

---
+++ SisAdminBancaJ3B/src/backend/controladores/ControladorCuentaHabiente.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend.controladores;
+
+import backend.database.ConexionBD;
+import backend.pojos.CuentaHabiente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jesfrin
+ */
+public class ControladorCuentaHabiente {
+
+    public static Connection connection = null;
+    private PreparedStatement prepState;
+    private ResultSet res;
+    //Consultas
+    private static final String INGRESO_DE_CUENTA_HABIENTE ="INSERT INTO CUENTA_HABIENTE VALUES(?,?,?,?,?,?,?,?)";
+    private static final String BUSQUEDA_DE_CUENTA_HABIENTES="SELECT * FROM CUENTA_HABIENTE";
+
+    public ControladorCuentaHabiente() {
+        connection = ConexionBD.getInstance();
+    }
+    
+    /**
+     * Registra un cuenta habiente, devuelve false si no se ha podido crear
+     * @param cuentaHabiente
+     * @return 
+     */
+    public boolean registroCuentaHabiente(CuentaHabiente cuentaHabiente){
+        try {
+            prepState = connection.prepareStatement(INGRESO_DE_CUENTA_HABIENTE);
+            prepState.setString(1, cuentaHabiente.getDpiCliente());
+            prepState.setString(2, cuentaHabiente.getNombres());
+            prepState.setString(3, cuentaHabiente.getApellidos());
+            prepState.setDate(4, cuentaHabiente.getFecha_nacimiento());
+            prepState.setString(5, cuentaHabiente.getDireccion());
+            prepState.setString(6, cuentaHabiente.getTelefono());
+            prepState.setString(7, cuentaHabiente.getCelular());
+            prepState.setString(8, cuentaHabiente.getEmail());
+            prepState.executeUpdate();
+            prepState.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
+    
+    public ArrayList<CuentaHabiente> busquedaDeCunetaHabientes(){
+        ArrayList<CuentaHabiente> cuentas = new ArrayList<>();
+        try {
+            prepState = connection.prepareStatement(BUSQUEDA_DE_CUENTA_HABIENTES);
+            res = prepState.executeQuery();
+            while(res.next()){
+                cuentas.add(new CuentaHabiente(res.getString(1), res.getString(2), res.getString(3), res.getDate(4), res.getString(5), res.getString(6), res.getString(7), res.getString(8)));
+            }
+            prepState.close();
+            res.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return cuentas;
+    }
+
+}
---
+++ SisAdminBancaJ3B/src/backend/pojos/CuentaHabiente.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend.pojos;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author jesfrin
+ */
+public class CuentaHabiente {
+    
+    private String dpiCliente;
+    private String nombres;
+    private String apellidos;
+    private Date fecha_nacimiento;
+    private String direccion;
+    private String telefono;
+    private String celular;
+    private String email;
+
+    
+    /**
+     * Para creacion y recuperacion
+     * @param dpiCliente
+     * @param nombres
+     * @param apellidos
+     * @param fecha_nacimiento
+     * @param direccion
+     * @param telefono
+     * @param celular
+     * @param email 
+     */
+    
+    public CuentaHabiente(String dpiCliente, String nombres, String apellidos, Date fecha_nacimiento, String direccion, String telefono, String celular, String email) {
+        this.dpiCliente = dpiCliente;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.celular = celular;
+        this.email = email;
+    }
+    
+    public String getDpiCliente() {
+        return dpiCliente;
+    }
+
+    public void setDpiCliente(String dpiCliente) {
+        this.dpiCliente = dpiCliente;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+}
