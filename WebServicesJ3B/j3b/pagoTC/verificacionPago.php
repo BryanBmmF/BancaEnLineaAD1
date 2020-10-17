0039371d2fa8048d05cc1059fc2e85503b8ef4d2
BryanBmmF <bryanmisael-monzonfuentes@cunoc.edu.gt>
2020-10-17 16:40:39 -0600

    Notificacion por correo de compra en linea completado

--- WebServicesJ3B/j3b/pagoTC/verificacionPago.php
+++ WebServicesJ3B/j3b/pagoTC/verificacionPago.php
@@ -7,7 +7,7 @@ $varYear = $_POST['year'];
 $varCodigoCVC = $_POST['codigocvc'];
 $varNombrePropietario = $_POST['nombre'];
 $varMontoPago = $_POST['montoPago'];
-$varFechaVencimiento = $varYear."-".$varMes."-01 00:00:00";
+$varFechaVencimiento = $varYear."-".$varMes."-31 00:00:00";
 
 $errorID = 0;
 //select no_tarjeta, tipo, estado, limite, deuda_actual, fecha_vencimiento, codigoCVC, tasa_interes, numero_cuenta, concat_ws(' ', nombres, apellidos) as nombre_completo from tarjeta a INNER JOIN cuenta_habiente b on a.dpi_cuenta_habiente = b.dpi_cliente where fecha_vencimiento > '2020-9-1' and estado='ACTIVA';
@@ -52,7 +52,7 @@ if ($num_rows > 0) {
         $insertTransaccionTarjeta = "UPDATE tarjeta SET deuda_actual='$nuevaDeudaActual' WHERE no_tarjeta = '$varNumTarjeta';";
         if ($conexion->query($insertTransaccionTarjeta) === true) {
           //header("location: ./asignacionExitosa.php");
-          header("location: ./pagoExitoso.php");
+          header("location: ./pagoExitoso.php?dpiCliente=$datoDpi&monto=$varMontoPago&tarjeta=$varNumTarjeta");
 
         } else {
           header("location: ./errorDeCompra.php?error=3");
